1d4375c72b84c0e33729406a5acc20bdcd019415
Matthew LaRocca <mlarocca.cpp@gmail.com>
2024-10-22 20:46:55 -0700

    add some test assets

---
+++ assets/rhythm/tabloid_junkie.js
@@ -0,0 +1,48 @@
+{
+	"duration": 3.0,
+	"time_offset": -0.3,
+	"events": [
+		{
+			"time": 0.03,
+			"type": "tap",
+			"columns": [0]
+		},
+		{
+			"time": 0.3,
+			"type": "tap",
+			"columns": [0]
+		},
+		{
+			"time": 0.57,
+			"type": "tap",
+			"columns": [1]
+		},
+
+		{
+			"time": 1.1,
+			"type": "tap",
+			"columns": [0]
+		},
+		{
+			"time": 1.38,
+			"type": "tap",
+			"columns": [0]
+		},
+		{
+			"time": 1.66,
+			"type": "tap",
+			"columns": [1]
+		},
+
+		{
+			"time": 2.19,
+			"type": "tap",
+			"columns": [0]
+		},
+		{
+			"time": 2.46,
+			"type": "tap",
+			"columns": [1]
+		}
+	]
+}
